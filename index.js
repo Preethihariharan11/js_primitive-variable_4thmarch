fb6818b03495668e725430644c929835484c8e73
Preethihariharan11 <preethihariit03@gmail.com>
2020-03-04 16:23:09 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 Primitive-strings,Number,Boolean,symbol
 Reference-Objects,array
 Vlues:special value-null,not-null,undefined,NaN*/
+//STRING METHODS//
 let intro="hai people this is "
 let fname='Preethi'
 let lname='Hariharan '
-//string defineition
+//-----------------string methods
 console.log(fname.length)
+//output=7
 //returns length of var
 console.log(intro.concat(fname,lname))
 console.log(fname.concat(lname))
@@ -45,3 +47,53 @@ console.log(intro.toUpperCase())
 //returns the string im upper case
 console.log(intro.trim())
 //removes whitespace from both sides of a string.
+
+//-------------------------------------------------------------------//
+//NUMBER METHODS//
+let age=18
+let salary= 4567.08967
+console.log(salary.toFixed(10))
+//toFixed() method converts a number into a string, keeping a specified number of decimals.
+console.log(salary.toExponential())
+//The toExponential() method converts a number into an exponential notation.
+console.log(salary.toString())
+//The toString() method converts a number to a string.
+console.log(salary.toString(2))
+console.log(salary.toString(8))
+console.log(salary.toString(16))
+//The toString() method converts a number to a string Of binary,octal,hexa-decimal.
+
+//----------------------------------------------------------------------//
+// GLOBAL METHODS//
+console.info("Hey hey!! I am a intern at Ducen")
+//The console.info() method writes a message to the console.
+console.warn("This is a warning!")
+//The console.warn() method writes a warning to the console.
+console.error("You made a mistake")
+//The console.error() method writes an error message to the console.
+console.table(["Audi", "Volvo", "Ford"])
+//The console.table() method writes a table in the console view.
+//-----few console methods------//
+//alert("Hello! I am an preethi's alert box!")
+//The alert() method displays an alert box with a specified message and an OK button.
+//prompt("Please enter your name","Preethi?!");
+//The prompt() method displays a dialog box that prompts the visitor for input.
+ // confirm("Confirm your details?");
+ //The confirm() method displays a dialog box with a specified message, along with an OK and a Cancel button.
+ console.log(Number(false))
+ //The Number() function converts the object argument to a number that represents the object's value.
+ console.log(isNaN('h8'))
+ //output=true
+ //The isNaN() function determines whether a value is an illegal number (Not-a-Number).
+console.log(parseFloat('10.764 yoyo'))
+//output=10.764
+//The parseFloat() function parses a string and returns a floating point number.
+console.log(parseInt(10.6745))
+//output=10
+//The parseInt() function parses a string and returns an integer.
+console.log(String(Boolean(1)))
+//output=true
+//The String() function converts the value of an object to a string.
+console.log(Boolean(1<2))
+//output=true
+// Boolean() function to find out if an expression (or a variable) is number or not respective to true or false
